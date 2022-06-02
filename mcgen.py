80fd80913077e7bffb775b5b54e99f35ce64192d
Clément <beudotc@gmail.com>
2022-06-02 17:02:22 +0200

    udpate world generation, no more storing world matrix
    need to fix add block and remove block tomorrow, then i can start generating the world based on player's position and movements

--- mcgen.py
+++ mcgen.py
@@ -50,13 +50,14 @@ persistence = 0.5
 lacunarity = 1.8
 #### TERRAIN GENERATION SETTINGS ####
 
-def createworld(chunk_amount, chunk_size):
-	cubes_positions = []
-	for x in range(chunk_size * chunk_amount):
-		cubes_positions.append([])
-		for y in range(chunk_size * chunk_amount):
-			cubes_positions[x].append([])
-			for z in range(chunk_size * chunk_amount):
+def buildmodel(vtx_layout, chunk_size, chunk_pos):
+	list_mats = []
+	final_list_mats = [[], [], []]
+	mdl_builder = hg.ModelBuilder()
+	for x in range(int(chunk_pos.x), int(chunk_pos.x + chunk_size)):
+		for y in range(int(chunk_pos.y), int(chunk_pos.y + chunk_size)):
+			for z in range(int(chunk_pos.z), int(chunk_pos.z + chunk_size)):
+				block_data = [False, 0]
 				v = noise.pnoise3(x/scale, 
                                     y/scale, z/scale,
                                     octaves=octaves, 
@@ -65,7 +66,7 @@ def createworld(chunk_amount, chunk_size):
                                     repeatx=1024, 
                                     repeaty=1024,
                                     repeatz=1024,  
-                                    base=15)
+                                    base=40)
 				material = 0
 				if v < -0.1:
 					material = 2 #water
@@ -75,55 +76,46 @@ def createworld(chunk_amount, chunk_size):
 				# 	v = -v
 				correcty = round(v * 100)
 				if y == correcty or y == correcty + 1 or y == correcty - 1:
-					cubes_positions[x][y].append([True, material])
-				elif y == 0:
-					cubes_positions[x][y].append([True, 2])
+					block_data = [True, material]
+				# elif y == 0:
+				# 	cubes_positions[x][y][z] = [True, 2]
 				else:
-					cubes_positions[x][y].append([False, 0])
-	return cubes_positions
-
-def buildmodel(vtx_layout, cubes_positions, chunk_size, chunk_pos):
-	list_mats = []
-	final_list_mats = [[], [], []]
-	mdl_builder = hg.ModelBuilder()
-	for x in range(int(chunk_pos.x), int(chunk_pos.x + chunk_size)):
-		for y in range(int(chunk_pos.y), int(chunk_pos.y + chunk_size)):
-			for z in range(int(chunk_pos.z), int(chunk_pos.z + chunk_size)):
-				list_mats.append([[x, y, z], cubes_positions[x][y][z]])
+					block_data = [False, 0]
+				list_mats.append([[x, y, z], block_data])
 	
 	list_mats.sort(key=lambda x: x[1][1])
 
 	for i in list_mats:
 		final_list_mats[i[1][1]].append(i)
 
-	# sort list_mats -> one list per material containing all cubes with this material
-	# endlist(material) after each list of cubes
 	for material in final_list_mats:
 		for cube in material:
 			x = cube[0][0]
 			y = cube[0][1]
 			z = cube[0][2]
 			position = hg.Vec3(x - chunk_pos.x, y - chunk_pos.y, z - chunk_pos.z)
-			should_draw = cubes_positions[x][y][z][0]
-			should_draw_xpositive = False
-			should_draw_xnegative = False
-			if x > 0 and x < len(cubes_positions) - 1:
-				should_draw_xpositive = cubes_positions[x + 1][y][z][0]
-				should_draw_xnegative = cubes_positions[x - 1][y][z][0]
-			should_draw_ypositive = False
-			should_draw_ynegative = False
-			if y > 0 and y < len(cubes_positions) - 1:
-				should_draw_ypositive = cubes_positions[x][y + 1][z][0]
-				should_draw_ynegative = cubes_positions[x][y - 1][z][0]
-			should_draw_zpositive = False
-			should_draw_znegative = False
-			if z > 0 and z < len(cubes_positions) - 1:
-				should_draw_zpositive = cubes_positions[x][y][z + 1][0]
-				should_draw_znegative = cubes_positions[x][y][z - 1][0]
-			is_covered_block = False
-			if should_draw_xpositive and should_draw_xnegative and should_draw_ypositive and should_draw_ynegative and should_draw_zpositive and should_draw_znegative:
-				is_covered_block = True
-			if should_draw and not is_covered_block:
+			should_draw = cube[1][0]
+
+			# should_draw_xpositive = False
+			# should_draw_xnegative = False
+			# if x > 0 and x < len(cubes_positions) - 1:
+			# 	should_draw_xpositive = cubes_positions[x + 1][y][z][0]
+			# 	should_draw_xnegative = cubes_positions[x - 1][y][z][0]
+			# should_draw_ypositive = False
+			# should_draw_ynegative = False
+			# if y > 0 and y < len(cubes_positions) - 1:
+			# 	should_draw_ypositive = cubes_positions[x][y + 1][z][0]
+			# 	should_draw_ynegative = cubes_positions[x][y - 1][z][0]
+			# should_draw_zpositive = False
+			# should_draw_znegative = False
+			# if z > 0 and z < len(cubes_positions) - 1:
+			# 	should_draw_zpositive = cubes_positions[x][y][z + 1][0]
+			# 	should_draw_znegative = cubes_positions[x][y][z - 1][0]
+			# is_covered_block = False
+			# if should_draw_xpositive and should_draw_xnegative and should_draw_ypositive and should_draw_ynegative and should_draw_zpositive and should_draw_znegative:
+			# 	is_covered_block = True
+
+			if should_draw:			# and not is_covered_block:
 				vertex0 = hg.Vertex()
 				vertex0.pos = hg.Vec3(-0.5 + position.x, -
 									  0.5 + position.y, -0.5 + position.z)
@@ -285,14 +277,14 @@ def buildmodel(vtx_layout, cubes_positions, chunk_size, chunk_pos):
 				d = mdl_builder.AddVertex(vertex3)
 				mdl_builder.AddTriangle(a, b, c)
 				mdl_builder.AddTriangle(a, c, d)
-		mdl_builder.EndList(cubes_positions[x][y][z][1])
+		mdl_builder.EndList(cube[1][1])
 
 	mdl = mdl_builder.MakeModel(vtx_layout)
 	return mdl
 
 def findchunkfromcoordinates(x, y, z, chunks, chunk_size, chunk_amount):
 	# start sanity checks
-	if x < 0 or y < 0 or z < 0:
+	if x < -(chunk_size * chunk_amount) or y < -(chunk_size * chunk_amount) or z < -(chunk_size * chunk_amount):
 		return None
 
 	if x > chunk_size * chunk_amount - 1 or y > chunk_size * chunk_amount - 1 or z > chunk_size * chunk_amount - 1:
@@ -440,16 +432,16 @@ def addblock(world, vtx_layout, chunks, chunk_amount, chunk_size, x, y, z):
 		chunks[int(chunk_x / chunk_size)][int(chunk_y  / chunk_size)][int(chunk_z  / chunk_size)][1] = mdl
 		reloadsidechunks(world, vtx_layout, chunks, chunk_amount, chunk_size, x, y, z)
 
-def generatechunks(chunk_amount, chunk_size, vtx_layout, world, chunk_index):
-	chunks = []
+def generatechunks(chunk_amount, chunk_index):
+	chunks = {}
 	queue = []
-	for curchunk_x in range(chunk_amount):
-		chunks.append([])
-		for curchunk_y in range(chunk_amount):
-			chunks[curchunk_x].append([])
-			for curchunk_z in range(chunk_amount):
+	for curchunk_x in range(-chunk_amount, chunk_amount):
+		chunks[curchunk_x] = {}
+		for curchunk_y in range(-chunk_amount, chunk_amount):
+			chunks[curchunk_x][curchunk_y] = {}
+			for curchunk_z in range(-chunk_amount, chunk_amount):
 				newchunk = None
-				chunks[curchunk_x][curchunk_y].append(newchunk)
+				chunks[curchunk_x][curchunk_y][curchunk_z] = newchunk
 				chunk_index += 1
 				queue.append([curchunk_x, curchunk_y, curchunk_z])
 	return chunks, queue
@@ -496,10 +488,9 @@ def show_preview_block(cam, vtx_layout_lines, vid_scene_opaque, pos_rgb):
 		hg.DrawLines(vid_scene_opaque, vtx, pos_rgb)
 
 chunk_size = 10
-chunk_amount = 15
+chunk_amount = 30
 chunk_generator_index = 0
-world = createworld(chunk_amount, chunk_size)
-chunks, queue = generatechunks(chunk_amount, chunk_size, vtx_layout, world, chunk_generator_index)
+chunks, queue = generatechunks(chunk_amount, chunk_size)
 
 # setup scene
 scene = hg.Scene()
@@ -518,11 +509,11 @@ back_lgt = hg.CreatePointLight(scene, hg.TranslationMat4(hg.Vec3(
 keyboard = hg.Keyboard()
 mouse = hg.Mouse()
 
-cam_pos = hg.Vec3(0, 1, -3)
+cam_pos = hg.Vec3(0, 100, 0)
 cam_rot = hg.Vec3(0, 0, 0)
 
 # main loop
-chunk_index = 0
+chunk_index = -chunk_amount
 frame = 0
 
 while not hg.ReadKeyboard().Key(hg.K_Escape):
@@ -537,7 +528,7 @@ while not hg.ReadKeyboard().Key(hg.K_Escape):
 	scene.Update(dt)
 
 	if chunk_index < len(queue):
-		mdl = buildmodel(vtx_layout, world, chunk_size, hg.Vec3(queue[chunk_index][0] * chunk_size, queue[chunk_index][1] * chunk_size, queue[chunk_index][2] * chunk_size))
+		mdl = buildmodel(vtx_layout, chunk_size, hg.Vec3(queue[chunk_index][0] * chunk_size, queue[chunk_index][1] * chunk_size, queue[chunk_index][2] * chunk_size))
 		mdl_ref = res.AddModel(str(chunk_index), mdl)
 		chunk_node = hg.CreateObject(scene, hg.TranslationMat4(hg.Vec3(queue[chunk_index][0] * chunk_size, queue[chunk_index][1] * chunk_size, queue[chunk_index][2] * chunk_size)), mdl_ref, [mat_cube, mat_sand, mat_water])
 		chunks[queue[chunk_index][0]][queue[chunk_index][1]][queue[chunk_index][2]] = [chunk_index, mdl, hg.Vec3(queue[chunk_index][0] * chunk_size, queue[chunk_index][1] * chunk_size, queue[chunk_index][2] * chunk_size), chunk_node]
